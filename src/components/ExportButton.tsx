d273496458082a072fb3a1fce823c5ebccde43b4
pedro <pedrokawa10@outlook.com>
2026-04-17 15:52:32 -0300

    style: changes in export button

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -22,6 +22,9 @@ export const ExportButton = ({ onClick, text = 'Exportar relatório', disabled =
                 backgroundColor: '#e67e22',
                 borderColor: '#E5E7EB',
                 color: '#ffffff',
+                width: '11rem',
+                height: '2.8rem',
+                fontSize: '1rem'
             //     '&:hover': {
             //     backgroundColor: '#F9FAFB',
             //     borderColor: '#D1D5DB',
