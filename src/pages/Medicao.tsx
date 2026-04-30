1e7561946e0de9f13f86b1c0d036601a3b51e83c
pedro <pedrokawa10@outlook.com>
2026-04-30 14:35:18 -0300

    feat: add medicao page and css tabs

--- src/pages/Medicao.tsx
+++ src/pages/Medicao.tsx
@@ -1,12 +1,90 @@
 import { Tabs, type TabItem } from '../components/Tabs';
+import { Button } from '@mui/material';
+// import { DataTable, type ColumnConfig } from "../components/DataTable";
+// // import { useState } from 'react';
+// interface Producao {
+//   data: string;
+//   rodovia: string;
+//   kmini: string;
+//   kmfim: string;
+//   extensao: number;
+//   pista: string;
+//   faixa: string;
+//   largura: number;
+//   producao: number;
+// }
 
 export const Medicao = () => {
+
+  // const [medicao, setMedicao] = useState<Producao[]>([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [termoBusca, setTermoBusca] = useState('');
+
+  // const colunas: ColumnConfig<Veiculo>[] = [
+  //       { key: 'codigoFrota', label: 'Cód. Frota', align: 'left' },
+  //       { key: 'placa', label: 'Placa', align: 'left' },
+  //       { 
+  //       key: 'modelo', 
+  //       label: 'Marca/Modelo',
+  //       align: 'left', 
+  //       // Um truque legal: juntar Marca e Modelo na mesma coluna para ficar mais limpo!
+  //       render: (row) => `${row.marca} ${row.modelo}` 
+  //       },
+  //       { key: 'anoModelo', label: 'Ano', align: 'left' },
+  //       { key: 'combustivel', label: 'Combustível', align: 'left' },
+  //       { 
+  //       key: 'status', 
+  //       label: 'Status', 
+  //       align: 'left',
+  //       render: (row) => (
+  //           <span style={{
+  //           padding: '0.25rem 0.75rem', borderRadius: '9999px', fontSize: '0.85rem', fontWeight: '500',
+  //           backgroundColor: row.status === 'Ativo' || row.status === 'Inativo' ? '#D1FAE5' : '#FEE2E2',
+  //           color: row.status === 'Ativo' || row.status === 'Inativo' ? '#065F46' : '#991B1B'
+  //           }}>
+  //           {row.status || 'Desconhecido'}
+  //           </span>
+  //       )
+  //       }
+  // ];
+
   const myAbas: TabItem[] = [
     {
       id: 'producao',
       label: 'Produção MRAF',
       // Aqui dentro você coloca a tabela inteira!
-      content: <div>Em breve: Gráficos de consumo por motorista vão entrar aqui!</div>
+      content: 
+      <>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'end'
+        }}>
+        {/* <h1>Em breve: Produção Diária</h1> */}
+        <Button 
+        variant="contained" 
+        color="primary"
+        // // startIcon={}
+        // onClick={onClick}
+        // disabled={disabled}
+        sx={{
+          textTransform: 'none',
+          borderRadius: '0.5rem',
+          fontWeight: 500,
+          backgroundColor: '#e67e22',
+          borderColor: '#E5E7EB',
+          color: '#ffffff',
+          width: '11rem',
+          height: '2.8rem',
+          fontSize: '1rem'}}
+        >Importar Produção
+        </Button>
+      </div>
+
+      {/* <div>
+        <DataTable></DataTable>
+      </div> */}
+          
+      </>
     },
     {
       id: 'emulsao',
