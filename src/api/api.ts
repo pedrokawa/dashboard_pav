b9d1ccf3c74714c829badda780737e3e96ba3c6a
pedro <pedrokawa10@outlook.com>
2026-05-15 17:02:35 -0300

    feat: add and fix api post abastecimento

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,17 @@
 const BASE_URL = import.meta.env.VITE_API_URL;   
+export interface Abastecimento {
+    placa: string;
+    marca: string;
+    modelo: string;
+    km: string;
+    horimetro?: string;
+    operador: string;
+    litros: string;
+    preco: string;
+    total: string;
+    posto: string;
+    dataAbastecimento: string;
+}
 
 export const api = {   
    login: async (user: string, password: string) => {
@@ -64,5 +77,22 @@ export const api = {
         }
 
         return await response.json();
+    },
+
+    postAbast: async ( dados: Abastecimento) => {
+      const response = await fetch(`${BASE_URL}/api/abastecimento`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados),
+    });
+
+    if (!response.ok){
+        const errorData = await response.json().catch(() => ({}));
+        throw new Error(errorData.message ||'Erro ao cadastrar abastecimento.');
+    }
+
+    return await response.json();
     }
 }
