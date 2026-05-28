2d351d715a6f85e932c5375eeb23ff588fbc8b93
pedro <pedrokawa10@outlook.com>
2026-05-28 11:37:00 -0300

    feat: create medicao types

---
+++ src/types/Medicao.ts
@@ -0,0 +1,14 @@
+export interface Medicao {
+    dataMedicao: string;
+    apontador: string;
+    rodovia: string;
+    sentido: string;
+    kmIni: number;
+    kmFim: number;
+    extensao: number;
+    largura: number;
+    faixa: string;
+    areaTotal: number;
+    observacoes: string;
+    foto: string[];
+}
