e826626ad5f784a4f654bcf1a8a5d5e9c6dc7374
pedro <pedrokawa10@outlook.com>
2026-06-09 08:33:42 -0300

    styles: change css in datepicker

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -4,9 +4,10 @@ interface DatePickerProps {
     value: string;
     onChange: (value: string) => void;
     label?: string;
+    disabled?: boolean;
 }
 
-export const DatePicker = ({ value, onChange, label }: DatePickerProps) => {
+export const DatePicker = ({ value, onChange, label, disabled }: DatePickerProps) => {
     return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '0.25rem' }}>
       {/* Só renderiza a label se você passar essa propriedade */}
@@ -19,18 +20,19 @@ export const DatePicker = ({ value, onChange, label }: DatePickerProps) => {
       <input
         type="date"
         value={value}
+        disabled={disabled}
         onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
         style={{
           padding: '0.5rem 0.75rem',
           fontSize: '0.95rem',
-          color: value ? '#111827' : '#9CA3AF', // Fica cinza se estiver vazio
-          backgroundColor: '#fff',
+          color: disabled ? "#6b7280" : (value ? '#111827' : '#9CA3AF'), // Fica cinza se estiver vazio
+          backgroundColor: disabled ? '#e5e7eb' : '#fff',
           border: '1px solid #D1D5DB',
           borderRadius: '0.5rem',
           outline: 'none',
           fontFamily: 'inherit',
           boxShadow: '0 1px 2px rgba(0, 0, 0, 0.05)',
-          cursor: 'pointer',
+          cursor: disabled ? 'not-allowed' : 'pointer',
           minWidth: '150px',
           height: '1.2rem'
         }}
